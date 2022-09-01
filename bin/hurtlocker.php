dc3702a143af24a1ee7df5042bddbe44733ca06c
Tim Otten <totten@civicrm.org>
2022-09-01 00:04:14 -0700

    Remove unused file

--- bin/hurtlocker.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Hurtlocker;
-
-require_once dirname(__DIR__) . '/vendor/autoload.php';
-
-$hl = new Hurtlocker();
-$exit = $hl->main(file_get_contents('php://stdin'));
-exit($exit);
