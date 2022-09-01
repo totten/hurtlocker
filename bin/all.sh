37d4b0cf6e27e5fb9b11bf4d131158956cbae685
Tim Otten <totten@civicrm.org>
2022-09-01 00:12:49 -0700

    Add bin/all.sh

---
+++ bin/all.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+###############################################################################
+## Bootstrap
+
+## Determine the absolute path of the directory with the file
+## usage: absdirname <file-path>
+function absdirname() {
+  pushd $(dirname $0) >> /dev/null
+    pwd
+  popd >> /dev/null
+}
+
+BINDIR=$(absdirname "$0")
+PRJDIR=$(dirname "$BINDIR")
+
+###############################################################################
+## Main
+
+for DBTYPE in dao pdo ; do
+for WORKERS in aaa aab abd ; do
+for N in 1 2 3 ; do
+  "$BINDIR"/hurtlocker.sh "$DBTYPE" "$WORKERS"
+done
+done
+done
--- bin/hurtlocker.sh
+++ bin/hurtlocker.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 ## usage: hurtlocker.sh <dbapi> <worker-config>
 ## example: hurtlocker.sh PDO aaa
